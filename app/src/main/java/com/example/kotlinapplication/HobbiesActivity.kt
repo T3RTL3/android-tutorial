858ae7c982f0629d32fbdd64c2197b2a8b7938fd
Karol Ziabski <karolziabski8@gmail.com>
2018-11-24 18:37:16 +0100

    Creating new Activity ( RecyclerView)

---
+++ app/src/main/java/com/example/kotlinapplication/HobbiesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class HobbiesActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hobbies_activity)
+    }
+}
